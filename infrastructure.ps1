44fac18e0edbfb6f9813934de3139b2eea1e97cd
Maxim Gorbatyuk <maxim.gorbatyuk@outlook.com>
2021-07-06 18:12:07 +0600

    Create infrastructure.ps1

---
+++ infrastructure.ps1
@@ -0,0 +1,9 @@
+# $env:USERPROFILE works only in windows and means %USERPROFILE%
+# https://devblogs.microsoft.com/scripting/powertip-use-powershell-to-find-user-profile-path/
+
+# https://docs.microsoft.com/en-us/aspnet/core/security/docker-compose-https?view=aspnetcore-3.1
+dotnet dev-certs https -ep $env:USERPROFILE\.aspnet\https\aspnetapp.pfx -p password
+dotnet dev-certs https --trust
+
+# Run the IdentityServer https://localhost:44393/
+docker-compose up --build database.identity elasticsearch rabbitmq
